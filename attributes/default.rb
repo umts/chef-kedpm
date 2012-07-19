96496410bca726b5037da9c327580d88507da9c2
Matt Moretti <werebus@gmail.com>
2012-07-19 09:46:18 -0400

    strings over hashes - meh

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default[:kedpm][:version] = "0.4.0"
-default[:kedpm][:url] = "http://sourceforge.net/projects/kedpm/files/kedpm/#{node[:kedpm][:version]}/kedpm-#{node[:kedpm][:version]}.tar.gz/download"
+default["kedpm"]["version"] = "0.4.0"
+default["kedpm"]["url"] = "http://sourceforge.net/projects/kedpm/files/kedpm/#{node["kedpm"]["version"]}/kedpm-#{node["kedpm"]["version"]}.tar.gz/download"
