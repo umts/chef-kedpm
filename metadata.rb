959bf7cc98d3d0632071aaec2cb0254139cfdb9a
Matt Moretti <werebus@gmail.com>
2014-01-21 15:51:13 -0500

    Style passes now

--- metadata.rb
+++ metadata.rb
@@ -1,23 +1,23 @@
-maintainer       "UMass Transit Service"
-maintainer_email "transit-mis@admin.umass.edu"
-license          "Apache 2.0"
-description      "Installs/Configures kedpm"
+maintainer       'UMass Transit Service'
+maintainer_email 'transit-mis@admin.umass.edu'
+license          'Apache 2.0'
+description      'Installs/Configures kedpm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          '0.0.2'
+name             'kedpm'
 
-depends          "python"
+depends          'python'
 
-attribute "kedpm/version",
-  :display_name => "KedPM version",
-  :description  => "The version of KedPM to install",
-  :type         => "string",
-  :required     => "required",
-  :default      => "0.4.0"
+attribute 'kedpm/version',
+          display_name: 'KedPM version',
+          description:  'The version of KedPM to install',
+          type:         'string',
+          required:     'required',
+          default:      '0.4.0'
 
-attribute "kedpm/url",
-  :display_name => "KedPM package URL",
-  :description  => "The URL for the KedPM tar.gz package",
-  :type         => "string",
-  :required     => "required",
-  :default      => "http://sourceforge.net/projects/kedpm/files/kedpm/0.4.0/kedpm-0.4.0.tar.gz/download""0.4.0"
-
+attribute 'kedpm/url',
+          display_name: 'KedPM package URL',
+          description:  'The URL for the KedPM tar.gz package',
+          type:         'string',
+          required:     'required',
+          default:      'http://sourceforge.net/projects/kedpm/files/kedpm/0.4.0/kedpm-0.4.0.tar.gz/download''0.4.0'
--- test/integration/default/serverspec/kedpm_spec.rb
+++ test/integration/default/serverspec/kedpm_spec.rb
@@ -5,6 +5,6 @@ include Serverspec::Helper::DetectOS
 
 describe 'the KedPM binary' do
   it 'exists' do
-    expect(command('which kedpm')).to return_stdout %r[bin/kedpm]
+    expect(command('which kedpm')).to return_stdout(/bin\/kedpm/)
   end
 end
