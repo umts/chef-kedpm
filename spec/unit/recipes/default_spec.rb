7ce01f9d91b519d43e291bd4def481cd54025d8d
Matt Moretti <werebus@gmail.com>
2015-04-22 10:31:07 -0400

    Some minor style edits

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe 'kedpm::default' do
-
   context 'When all attributes are default, on an unspecified platform' do
-
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
       runner.converge(described_recipe)
--- test/integration/default/serverspec/kedpm_spec.rb
+++ test/integration/default/serverspec/kedpm_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'kedpm::default' do
   describe command('pip list') do
-    its(:stdout) { should match /kedpm/ }
+    its(:stdout) { should match(/kedpm/) }
   end
   describe command('which kedpm') do
     its(:exit_status) { should eq 0 }
